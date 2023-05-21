958ee14dad9f2bec266003f4e47a970f3b6b5835
Byte Cats <devin@bytecats.codes>
2023-05-21 12:04:03 -0500

    fixed the cat filter

--- src/lib/CategoryFilters.svelte
+++ src/lib/CategoryFilters.svelte
@@ -5,7 +5,7 @@
     const dispatch = createEventDispatcher();
     let filters = {};
     let isExpanded = false;
-    let clickedCategory = new Map()
+    let clickedCategory = new Map();
 
     function updateFilters() {
         dispatch("updateFilters", filters);
@@ -20,6 +20,12 @@
         clickedCategory = category.name;
         updateFilters();
     }
+
+    function getRandomCategory() {
+        return categories[Math.floor(Math.random() * categories.length)];
+    }
+
+    let randomCategory = getRandomCategory();
 </script>
 
 <style>
@@ -71,23 +77,64 @@
     .filter-item.unchecked {
         background-color: #664d70;
     }
+
+    .toggle-button {
+        display: block;
+        text-align: center;
+        background-color: #4a4a4a;
+        color: #fff;
+        font-size: 14px;
+        padding: 8px 12px;
+        cursor: pointer;
+    }
+
+    .toggle-button {
+        display: flex;
+        align-items: center;
+        background-color: #664d70;
+        border-radius: 20px;
+        padding: 8px 12px;
+        max-width: 200px;
+        font-size: 14px;
+        color: #fff;
+        transition: background-color 0.3s;
+    }
+
+    .toggle-button:hover {
+        background-color: #4a4a4a;
+    }
 </style>
 
 <div class="filters-container" class:collapsed="{!isExpanded}">
     <div class="filters">
+        <div class="toggle-button" on:click="{toggleFilters}">
+            {isExpanded ? 'Hide filters' : 'Show filters'}
+        </div>
+        <label
+                class="filter-item {clickedCategory === randomCategory.name ? 'clicked' : ''}"
+                on:click="{() => toggleCheckbox(randomCategory)}"
+        >
+            <input
+                    type="checkbox"
+                    bind:checked="{filters[randomCategory.name]}"
+                    style="display: none;"
+            />
+            <span>{randomCategory.title}</span>
+        </label>
         {#each categories as category}
-            <label
-                    class="filter-item {clickedCategory === category.name ? 'clicked' : ''}"
-                    on:click="{() => toggleCheckbox(category)}"
-            >
-                <input
-                        type="checkbox"
-                        bind:checked="{filters[category.name]}"
-                        style="display: none;"
-                />
-                <span>{category.title}</span>
-            </label>
+            {#if category.name !== randomCategory.name}
+                <label
+                        class="filter-item {clickedCategory === category.name ? 'clicked' : ''}"
+                        on:click="{() => toggleCheckbox(category)}"
+                >
+                    <input
+                            type="checkbox"
+                            bind:checked="{filters[category.name]}"
+                            style="display: none;"
+                    />
+                    <span>{category.title}</span>
+                </label>
+            {/if}
         {/each}
     </div>
-    <span class="toggle-button" on:click="{toggleFilters}">{isExpanded ? 'Hide' : 'Show'} filters</span>
 </div>
